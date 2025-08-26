f44bc1ae8b70ee3550fffcc4b3c9185ce7bdd115
Bruno-DarkSouler <brunofornasar@gmail.com>
2025-08-25 21:38:16 -0300

    instancias inicio_base

---
+++ escritorio/tarjeta_comida_principal.cpp
@@ -0,0 +1,18 @@
+#include "tarjeta_comida_principal.h"
+#include "ui_tarjeta_comida_principal.h"
+
+tarjeta_comida_principal::tarjeta_comida_principal(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::tarjeta_comida_principal)
+{
+    ui->setupUi(this);
+}
+
+tarjeta_comida_principal::~tarjeta_comida_principal()
+{
+    delete ui;
+}
+
+void tarjeta_comida_principal::setData(const DataModel data){
+    ui->label_2->setText(data);
+}
---
+++ escritorio/tarjeta_comida_principal.h
@@ -0,0 +1,23 @@
+#ifndef TARJETA_COMIDA_PRINCIPAL_H
+#define TARJETA_COMIDA_PRINCIPAL_H
+
+#include <QWidget>
+
+namespace Ui {
+class tarjeta_comida_principal;
+}
+
+class tarjeta_comida_principal : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit tarjeta_comida_principal(QWidget *parent = nullptr);
+    ~tarjeta_comida_principal();
+    void setData(string texto);
+
+private:
+    Ui::tarjeta_comida_principal *ui;
+};
+
+#endif // TARJETA_COMIDA_PRINCIPAL_H
